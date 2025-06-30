bbedd5807b77c2d25e95662619efe483280935f5
Lucasfi <lukafigueroa64@gmail.com>
2025-06-30 11:33:50 -0300

    agregamos estilo a boton en la card y agregamos imagen de ejemplo para las noticias

--- src/components/ListaNoticias.jsx
+++ src/components/ListaNoticias.jsx
@@ -1,27 +1,24 @@
+const imagen =
+  "https://cloudfront-us-east-1.images.arcpublishing.com/infobae/7TZCY4E3D5DB5GCTB7IZGFK66M.jpg";
+
 const ListaNoticia = () => {
   return (
     <div className="card p-1 mx-3">
-      <img
-        src=""
-        className="card-img-top"
-        alt=""
-      />
-      <div className="card-body p-1">
-        <h6 className="card-header"></h6>
-        <p className="card-text truncate-3-lines">
-          dsdsd
-        </p>
-        <div className="d-flex flex-column mb-0">
-          <span>
-            Pais: <strong>sdsdsdd</strong>
-          </span>
-          <span>
-            Publicacion: <strong>sdsdsdsd</strong>
-          </span>
-        </div>
+      <div className="card-header text-center">
+        <h6>
+          Pais: <strong>sdsdsdd</strong>
+        </h6>
       </div>
-      <div className="card-footer d-flex justify-content-center">
-        <a className="botonVerNoticia text-white" target="blank" >Ver noticia completa</a>
+      <img src={imagen} className="card-img-top" alt="" />
+      <div className="card-body p-1">
+        <h6 className="card-header">Titulo</h6>
+        <p className="card-text truncate-3-lines">Description:</p>
+      </div>
+      <div className="card-footer d-flex flex-column justify-content-center">
+        <a className="botonVerNoticia text-white" target="blank">
+          Ver noticia completa
+        </a>
+        <p class="card-text"><small class="text-body-secondary">Publicacion</small></p>
       </div>
     </div>
   );
